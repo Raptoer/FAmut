4b7d2819e8c3df438a311f1811675fffe0bc0906
Pooter <raptoer@gmail.com>
2017-09-04 23:09:10 -0700

    Adds logfile Adds binary files to ignore list

--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -23,19 +25,30 @@ public class main {
         Files.list(inputFile).filter(p -> p.getFileName().toString().endsWith(".pbo")).forEach(
                 pbo -> {
                     String tempFolder = outputFile.toAbsolutePath().toString() + "\\" + pbo.getFileName().toString().replace(".pbo", "");
+                    String logFile = outputFile.toAbsolutePath().toString() + "\\" + pbo.getFileName().toString().replace(".pbo", ".log");
+                    BufferedWriter logOut = null;
+                    try {
+                        logOut = new BufferedWriter(new FileWriter(new File(logFile)));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     String[] cmd = { "C:\\apps\\PBO Manager v.1.4 beta\\PBOConsole.exe", "-unpack", pbo.toString(), tempFolder};
                     Process p = null;
                     try {
-                        p = Runtime.getRuntime().exec(cmd);
+                        ProcessBuilder pb = new ProcessBuilder(cmd);
+                        pb.inheritIO();
+                        p = pb.start();
                         p.waitFor();
                         Path primary = Paths.get(tempFolder);
                         Stream<Path> existingFiles = getChildren(primary);
-                        existingFiles.filter(e -> !e.equals(primary)).forEach( existingFile -> {
+                        BufferedWriter finalLogOut = logOut;
+                        existingFiles.sorted(Comparator.comparing(Path::getNameCount).reversed()).filter(e -> !e.equals(primary)).forEach(existingFile -> {
                             try {
                                 Path shortened = primary.relativize(existingFile);
                                 if(Files.exists(oldFramework.resolve(shortened))
                                         && !Files.exists(ignoreFile.resolve(shortened))) {
                                     Files.delete(Paths.get(tempFolder + "\\" + shortened.toString()));
+                                    finalLogOut.write("\ndeleting: " + shortened);
                                 }
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -47,6 +60,7 @@ public class main {
                                 Path shortened = newFramework.relativize(newFile);
                                 if(!Files.exists(ignoreFile.resolve(shortened))) {
                                     Files.copy(newFile, Paths.get(tempFolder + "\\" + shortened.toString()));
+                                    finalLogOut.write("\ncopying: " + shortened);
                                 }
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -58,7 +72,8 @@ public class main {
                         replaceFiles.filter(e -> !e.equals(replaceTemplates)).forEach( replaceFile -> {
                             try {
                                 Path shortened = replaceTemplates.relativize(replaceFile);
-                                if(Files.exists(Paths.get(tempFolder).resolve(shortened))) {
+                                if(Files.exists(Paths.get(tempFolder).resolve(shortened))
+                                        && !Files.isDirectory(replaceFile)) {
                                     List<String> replaceLines = Files.readAllLines(replaceFile);
                                     StringJoiner replaceLinesBunched = new StringJoiner("\r\n");
                                     replaceLines.forEach(replaceLinesBunched::add);
@@ -69,7 +84,13 @@ public class main {
                                     toBeReplaceLines.forEach(toBeReplaceLinesBunched::add);
                                     String toBeReplacedJoined = toBeReplaceLinesBunched.toString();
                                     for(int i = 0; i < pieces.length; i = i + 2) {
+                                        String pieceNum = pieces[i].substring(0, pieces[i].indexOf("|"));
+                                        pieces[i] = pieces[i].substring(pieces[i].indexOf("|")+1);
+                                        String toBeReplacedJoinedPre = toBeReplacedJoined;
                                         toBeReplacedJoined = toBeReplacedJoined.replace(pieces[i], pieces[i+1]);
+                                        if(!toBeReplacedJoinedPre.equals(toBeReplacedJoined)) {
+                                            finalLogOut.write("\nReplaceNum: " + pieceNum + " was used");
+                                        }
                                     }
                                     Files.write(Paths.get(tempFolder).resolve(shortened), Arrays.asList(toBeReplacedJoined), StandardOpenOption.TRUNCATE_EXISTING);
                                 }
@@ -78,10 +99,11 @@ public class main {
                             }
                         });
 
-                        String pboOutput = outputFile.toAbsolutePath().toString() + "\\" + (pbo.getFileName().toString() + "U").replace(".pbo", "") + ".pbo";
+                        String pboOutput = outputFile.toAbsolutePath().toString() + "\\" + (pbo.getFileName().toString()).replaceAll("v(\\d+)", "v$1U");
                         String[] outputCmd = { "C:\\apps\\PBO Manager v.1.4 beta\\PBOConsole.exe", "-pack", tempFolder, pboOutput};
-                        p = Runtime.getRuntime().exec(outputCmd);
-                        p.waitFor();
+                        pb = new ProcessBuilder(outputCmd);
+                        pb.inheritIO();
+                        p = pb.start();
 
 
                     } catch (IOException | InterruptedException e) {
